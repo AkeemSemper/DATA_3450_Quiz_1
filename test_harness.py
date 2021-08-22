cbe51738912b237bfcd9459f0e4107b598eace6a
AkeemSemper <55889346+AkeemSemper@users.noreply.github.com>
2021-08-22 10:50:52 -0600

    added remote gist

--- test_harness.py
+++ test_harness.py
@@ -1,76 +1,52 @@
+#Import Python Stuff
 import thinkplot
 import thinkstats2
-
 import pandas as pd
+import numpy as np
+
+#Import solution file
 import import_ipynb
 import solution_file
 
+#Load data file
 with open("LabourTrainingEvaluationData.csv") as f:
     data = pd.read_csv(f)
-#print(data.dtypes)
-#print(data["Earnings_1978"].describe())
+
+#Import remote solutions
+import httpimport
+from httpimport import *
+url = "https://gist.githubusercontent.com/AkeemSemper/5efb8626f92408eb17149905bf564d92/raw/5d250b0129aa534b39f5e384c3417ab9c35aa4e9/"
+with httpimport.remote_repo(["stats_Quiz1_sol"], url):
+    import stats_Quiz1_sol
 
 def test_rangeMag(df_in, columnName):
-    usedColumn = df_in[columnName]
-    tmpMax = usedColumn.max()
-    tmpMin = usedColumn.min()
-    retVal = tmpMax-tmpMin
-    #print(tmpMin)
-    #print(tmpMax)
-    #print(retVal)
-    assert (retVal == solution_file.rangeMag(data, columnName))
+    studAnswer = solution_file.rangeMag(df_in, columnName)
+    solAnswer = stats_Quiz1_sol.rangeMag(df_in, columnName)
+    assert (studAnswer == solAnswer)
 
 def test_earnedAbove(df_in, amount, columnName):
-    usedColumn = df_in[columnName]
-    #print(usedColumn)
-    biggerList = usedColumn[usedColumn>amount]
-    #print(biggerList)
-    retVal = biggerList.size
-    #print(retVal)
-    assert (retVal == (solution_file.earnedAbove(data, amount, columnName)))
+    studAnswer = solution_file.earnedAbove(df_in, amount, columnName)
+    solAnswer = stats_Quiz1_sol.earnedAbove(df_in, amount, columnName)
+    assert (studAnswer == solAnswer)
 
-def test_difference_74_78(df_in):
-    incomes = df_in[["Earnings_1974", "Earnings_1978"]]
-    #print(incomes.dtypes)
-    a = (incomes["Earnings_1978"] - incomes["Earnings_1974"])
-    #print(a)
-    retVal = np.mean(a)
-    #print(np.mean(a))
-    #print(df_in)
-    assert (retVal == solution_file.difference_74_78(data))
+def test_difference_74_78(df_in, column_low, column_high):
+    studAnswer = solution_file.difference_74_78(df_in, column_low, column_high)
+    solAnswer = stats_Quiz1_sol.difference_74_78(df_in, column_low, column_high)
+    assert (studAnswer == solAnswer)
 
 def test_ageCount(df_in, low_age, high_age):
-    numData = pd.to_numeric(df_in["Age"], errors='raise')
-    numData = np.array(numData)
-    #print(numData)
-    data1 = numData[np.where((numData <= high_age) & (numData >= low_age))]
-    retVal = data1.size
-    #print(retVal)
-    #return retVal
-    assert (retVal == solution_file.ageCount(data))
+    studAnswer = solution_file.ageCount(df_in, low_age, high_age)
+    solAnswer = stats_Quiz1_sol.ageCount(df_in, low_age, high_age)
+    assert (studAnswer == solAnswer)
 
-def test_cohort(df_in):
-    df_in["Cohort"] = (df_in["Race"]+df_in["Hisp"]+df_in["MaritalStatus"])
-    #print(df_in["Cohort"].unique())
-    cohortList = df_in["Cohort"].unique()
-    i = 0
-    resList = []
-    lowest = -1
-    while i < cohortList.size:
-        tmp = df_in[df_in.Cohort == cohortList[i]]
-        #print(tmp.shape[0])
-        #print(df_in[df_in.Cohort == cohortList[i]])
-        #cohortSize = tmp.shape[0]
-        cohortMed = np.median(pd.to_numeric(tmp["Earnings_1978"]))
-        tmpTuple = (cohortList[i], cohortMed)
-        resList.append(tmpTuple)
-        if ((cohortMed < lowest) | (lowest == -1)):
-            lowest = cohortMed
-        i = i + 1
-    assert (lowest == solution_file.cohort(data))
+def test_cohort(df_in, cohort1, cohort2, cohort3, earningsColumn):
+    studAnswer = solution_file.cohort(df_in, cohort1, cohort2, cohort3, earningsColumn)
+    solAnswer = stats_Quiz1_sol.cohort(df_in, cohort1, cohort2, cohort3, earningsColumn)
+    assert (studAnswer == solAnswer)
 
-# test_ageCount(data, )
-# test_average_1(data)
-# test_cohort(data)
-#test_rangeMag(data,"Earnings_1978")
+#
+#test_rangeMag(data, "Age")
 #test_earnedAbove(data, 19000, "Earnings_1978")
+#test_difference_74_78(data, "Earnings_1974", "Earnings_1978")
+#test_ageCount(data, 18, 35)
+#test_cohort(data, "Race", "Hisp", "MaritalStatus", "Earnings_1978")
