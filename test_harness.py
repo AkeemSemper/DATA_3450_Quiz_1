25691abe57e186592483281a41e2c49354ecf7bd
AkeemSemper <55889346+AkeemSemper@users.noreply.github.com>
2021-08-22 11:47:01 -0600

    fixed harness

--- test_harness.py
+++ test_harness.py
@@ -19,27 +19,46 @@ url = "https://gist.githubusercontent.com/AkeemSemper/5efb8626f92408eb17149905bf
 with httpimport.remote_repo(["stats_Quiz1_sol"], url):
     import stats_Quiz1_sol
 
-def test_rangeMag(df_in, columnName):
-    studAnswer = solution_file.rangeMag(df_in, columnName)
-    solAnswer = stats_Quiz1_sol.rangeMag(df_in, columnName)
+#Set Test Variables
+df_in = data #data source
+#Range
+columnNameRange = "Earnings_1978"
+#EarnedAbove
+amount = 19000
+columnNameAbove = "Earnings_1978"
+#Difference
+column_low = "Earnings_1974"
+column_high = "Earnings_1978"
+#Age Count
+low_age = 18
+high_age = 35
+#cohort
+cohort1 = "Race"
+cohort2 = "Hisp"
+cohort3 = "MaritalStatus"
+earningsColumn = "Earnings_1978"
+
+def test_rangeMag():
+    studAnswer = solution_file.rangeMag(df_in, columnNameRange)
+    solAnswer = stats_Quiz1_sol.rangeMag(df_in, columnNameRange)
     assert (studAnswer == solAnswer)
 
-def test_earnedAbove(df_in, amount, columnName):
-    studAnswer = solution_file.earnedAbove(df_in, amount, columnName)
-    solAnswer = stats_Quiz1_sol.earnedAbove(df_in, amount, columnName)
+def test_earnedAbove():
+    studAnswer = solution_file.earnedAbove(df_in, amount, columnNameAbove)
+    solAnswer = stats_Quiz1_sol.earnedAbove(df_in, amount, columnNameAbove)
     assert (studAnswer == solAnswer)
 
-def test_difference_74_78(df_in, column_low, column_high):
+def test_difference_74_78():
     studAnswer = solution_file.difference_74_78(df_in, column_low, column_high)
     solAnswer = stats_Quiz1_sol.difference_74_78(df_in, column_low, column_high)
     assert (studAnswer == solAnswer)
 
-def test_ageCount(df_in, low_age, high_age):
+def test_ageCount():
     studAnswer = solution_file.ageCount(df_in, low_age, high_age)
     solAnswer = stats_Quiz1_sol.ageCount(df_in, low_age, high_age)
     assert (studAnswer == solAnswer)
 
-def test_cohort(df_in, cohort1, cohort2, cohort3, earningsColumn):
+def test_cohort():
     studAnswer = solution_file.cohort(df_in, cohort1, cohort2, cohort3, earningsColumn)
     solAnswer = stats_Quiz1_sol.cohort(df_in, cohort1, cohort2, cohort3, earningsColumn)
     assert (studAnswer == solAnswer)
