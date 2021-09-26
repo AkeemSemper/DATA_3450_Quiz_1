ec4c5b6101a24d95b9029643020aafe74212323a
AkeemSemper <55889346+AkeemSemper@users.noreply.github.com>
2021-09-26 17:19:11 -0600

    updates

--- test_harness.py
+++ test_harness.py
@@ -4,6 +4,7 @@ import thinkstats2
 import pandas as pd
 import numpy as np
 import pytest
+import math
 
 #Import solution file
 import import_ipynb
@@ -44,35 +45,40 @@ earningsColumn = "Earnings_1978"
 def test_rangeMag():
     studAnswer = solution_file.rangeMag(df_in, columnNameRange)
     solAnswer = stats_Quiz1_sol.rangeMag(df_in, columnNameRange)
-    assert (studAnswer == solAnswer)
+    assert math.isclose(studAnswer, solAnswer, rel_tol=.05)
+    #assert (studAnswer == solAnswer)
 
 @pytest.mark.b
 def test_earnedAbove():
     studAnswer = solution_file.earnedAbove(df_in, amount, columnNameAbove)
     solAnswer = stats_Quiz1_sol.earnedAbove(df_in, amount, columnNameAbove)
-    assert (studAnswer == solAnswer)
+    assert math.isclose(studAnswer, solAnswer, rel_tol=.05)
+    #assert (studAnswer == solAnswer)
 
 @pytest.mark.c
 def test_difference_74_78():
     studAnswer = solution_file.difference_74_78(df_in, column_low, column_high)
     solAnswer = stats_Quiz1_sol.difference_74_78(df_in, column_low, column_high)
-    assert (studAnswer == solAnswer)
+    assert math.isclose(abs(studAnswer), abs(solAnswer), rel_tol=.05)
+    #assert (studAnswer == solAnswer)
 
 @pytest.mark.d
 def test_ageCount():
     studAnswer = solution_file.ageCount(df_in, columnNameAge, low_age, high_age)
     solAnswer = stats_Quiz1_sol.ageCount(df_in, columnNameAge, low_age, high_age)
-    assert (studAnswer == solAnswer)
+    assert math.isclose(studAnswer, solAnswer, rel_tol=.05)
+    #assert (studAnswer == solAnswer)
 
 @pytest.mark.e
 def test_cohort():
     studAnswer = solution_file.cohort(df_in, cohort1, cohort2, cohort3, earningsColumn)
     solAnswer = stats_Quiz1_sol.cohort(df_in, cohort1, cohort2, cohort3, earningsColumn)
-    assert (studAnswer == solAnswer)
+    assert math.isclose(studAnswer, solAnswer, rel_tol=.05)
+    #assert (studAnswer == solAnswer)
 
 #
-#test_rangeMag(data, "Age")
-#test_earnedAbove(data, 19000, "Earnings_1978")
-#test_difference_74_78(data, "Earnings_1974", "Earnings_1978")
-#test_ageCount(data, 18, 35)
-#test_cohort(data, "Race", "Hisp", "MaritalStatus", "Earnings_1978")
+test_rangeMag()
+test_earnedAbove()
+test_difference_74_78()
+test_ageCount()
+test_cohort()
